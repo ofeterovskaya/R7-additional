f1e9dc3ef0899f12ac8b59e4df47c5b0263209bf
OksanaCTD <fetter.oksana@gmail.com>
2024-09-17 23:33:01 -0400

    added a table into index

--- app/models/order_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Order, type: :model do
-  it { should belong_to(:customer) }
-  
-  it { should validate_presence_of(:product_name) }
-  it { should validate_presence_of(:product_count) }
-  it { should validate_presence_of(:customer) }
-end
